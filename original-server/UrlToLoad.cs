c3eb5c66dbec858ceb7338c9364b1a82056842e8
Wolmar Parser <parser@wolmar.local>
2025-10-06 14:38:06 +0300

    Add original BrowserProxy code with .NET 8.0 and localhost configuration

---
+++ original-server/UrlToLoad.cs
@@ -0,0 +1,8 @@
+namespace BrowserProxy;
+
+public class UrlToLoad
+{
+    public string Url { get; set; }
+    public string? WaitSelector { get; set; }
+    public string? ClickSelector { get; set; }
+}
